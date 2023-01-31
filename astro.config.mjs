f9c8a76ce121ac511d4299e6407b82dd61d96001
Muhammad Reza Zulman <m.rezazulman@gmail.com>
2023-01-31 09:49:16 +0700

    fix github pages config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://peerac.github.io',
-    base: '/peerac-site',
+    site: 'https://peerac.github.io'
 });
